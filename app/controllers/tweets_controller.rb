b8844cf3c3668e0ea8eaadd45a45882384cd2900
siann55 <takodo_exword22@yahoo.co.jp>
2019-05-06 15:41:39 +0900

    編集・削除機能

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -14,6 +14,7 @@ class TweetsController < ApplicationController
   def destroy
     tweet = Tweet.find(params[:id])
     tweet.destroy if tweet.user_id == current_user.id
+    redirect_to action: :index
   end
 
   def edit
